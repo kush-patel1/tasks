5360beb8f1a8ac3ca2294a4d51e848f0a6539a0f
kushpatel <kushptl@udel.edu>
2024-09-05 21:36:12 -0400

    TASK 3 COMPLETE

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./App.css";
+import { Button } from "react-bootstrap";
 
 function App(): React.JSX.Element {
     return (
@@ -7,11 +8,41 @@ function App(): React.JSX.Element {
             <header className="App-header">
                 UD CISC275 with React Hooks and TypeScript | Kush Patel
             </header>
-            <p>Hello World!</p>
+            <h1>Computer Science Fields</h1>
+            <img
+                src="/Users/kushpatel/Downloads/image1.jpg"
+                alt="A brain map of Computer Science"
+            />
+            <p>Rankings are based on median salary. Highest to Lowest.</p>
+            <p>
+                source:
+                https://graduate.northeastern.edu/resources/top-paying-computer-science-jobs/
+            </p>
+            <ol>
+                <li>Software architect</li>
+                <li>Security engineer</li>
+                <li>Software developer</li>
+                <li>Computer scientist</li>
+                <li>Software development engineer</li>
+            </ol>
+            <Button
+                onClick={() => {
+                    console.log("Hello World!");
+                }}
+            >
+                Log Hello World
+            </Button>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload.
             </p>
+            <div
+                style={{
+                    width: "1512px",
+                    height: "50px",
+                    backgroundColor: "red",
+                }}
+            ></div>
         </div>
     );
 }
